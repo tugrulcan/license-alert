0b33437f4762cb76d7fb29e6b4ce66b4fd143190
Tugrul Can <tugrulcansollu@gmail.com>
2017-04-08 07:24:43 +0300

    Project files added

---
+++ src/eventPage.js
@@ -0,0 +1,261 @@
+
+function f1 (request, sender, sendResponse) {
+    if(request.action == "show")
+    {
+        chrome.tabs.query({active:true, currentWindow:true}, function (tabs) {
+            chrome.pageAction.show(tabs[0].id);
+
+            var url = tabs[0].url;
+            url = url.split('/');
+            var owner = url[3].toString();
+            var repo = url[4].toString();
+            var owner_repo = owner + "/" + repo;
+            var license = null;
+            var license_dict = null;
+
+
+            chrome.storage.sync.get("license_dict", function (items) {
+                if(items.license_dict)
+                {
+                    license_dict = items.license_dict;
+                    if(license_dict[owner_repo] != undefined)
+                    {
+                        license = license_dict[owner_repo];
+                        chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                    }else{
+                        var apiUrl=  "https://api.github.com/repos/" + owner_repo + "/license";
+                        var json = null;
+                        var xhr = new XMLHttpRequest();
+
+                        xhr.onload = function() {
+                            if (this.status === 404) {
+                                // license not found
+                                license = "notFound";
+                                chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                                license_dict[owner_repo] = license;
+                                chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+
+                            }}
+                        xhr.open("GET", apiUrl, true);
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState == 4) {
+                                json = JSON.parse(xhr.response);
+                                license = json.license;
+                                chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                                license_dict[owner_repo] = license;
+                                chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+                            }
+                        }
+                        xhr.send();
+                    }
+                }else{
+                    license_dict = {};
+                    var apiUrl=  "https://api.github.com/repos/" + owner_repo + "/license";
+                    var json = null;
+                    var xhr = new XMLHttpRequest();
+
+                    xhr.onload = function() {
+                        if (this.status === 404) {
+                            // license not found
+                            license = "notFound";
+                            chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                            license_dict[owner_repo] = license;
+                            chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+
+                        }}
+                    xhr.open("GET", apiUrl, true);
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState == 4) {
+                            json = JSON.parse(xhr.response);
+                            license = json.license;
+                            chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                            license_dict[owner_repo] = license;
+                            chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+                        }
+                    }
+                    xhr.send();
+
+                }
+            });
+        });
+    }
+}
+
+chrome.runtime.onMessage.addListener(f1);
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.query({active:true, currentWindow:true}, function (tabs) {
+        chrome.pageAction.show(tabs[0].id);
+        var url = tabs[0].url;
+        url = url.split('/');
+        var owner = url[3].toString();
+        var repo = url[4].toString();
+        var owner_repo = owner + "/" + repo;
+        var license = null;
+        var license_dict = null;
+
+
+        chrome.storage.sync.get("license_dict", function (items) {
+            if(items.license_dict)
+            {
+                license_dict = items.license_dict;
+                if(license_dict[owner_repo] != undefined)
+                {
+                    //alert(owner_repo +  " license found in cache!");
+                    license = license_dict[owner_repo];
+                    //alert("license: " + license);
+                    chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                }else{
+                    //alert(owner_repo +  " license not found api call!");
+                    var apiUrl=  "https://api.github.com/repos/" + owner_repo + "/license";
+                    var json = null;
+                    var xhr = new XMLHttpRequest();
+
+                    xhr.onload = function() {
+                        if (this.status === 404) {
+                            // license not found
+                            license = "notFound";
+                            chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                            license_dict[owner_repo] = license;
+                            chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+
+                        }else if (this.status === 403) {
+                            // API request limit exceed
+                            license = "limitExceed";
+                            chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                        }};
+                    xhr.open("GET", apiUrl, true);
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState == 4) {
+                            json = JSON.parse(xhr.response);
+                            license = json.license;
+                            chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                            license_dict[owner_repo] = license;
+                            chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+                        }
+                    }
+                    xhr.send();
+                }
+            }else{
+                license_dict = {};
+                //alert(owner_repo +  " license not found api call!");
+                var apiUrl=  "https://api.github.com/repos/" + owner_repo + "/license";
+                var json = null;
+                var xhr = new XMLHttpRequest();
+
+                xhr.onload = function() {
+                    if (this.status === 404) {
+                        // license not found
+                        license = "notFound";
+                        chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                        license_dict[owner_repo] = license;
+                        chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+
+                    }}
+                xhr.open("GET", apiUrl, true);
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4) {
+                        json = JSON.parse(xhr.response);
+                        license = json.license;
+                        chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                        license_dict[owner_repo] = license;
+                        chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+                    }
+                }
+                xhr.send();
+
+            }
+        });
+    });
+
+});
+
+
+
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    if(details.frameId === 0) {
+        // Fires only when details.url === currentTab.url
+        chrome.tabs.get(details.tabId, function(tab) {
+            if(tab.url === details.url) {
+                chrome.tabs.query({active:true, currentWindow:true}, function (tabs) {
+                    chrome.pageAction.show(tabs[0].id);
+
+                    var url = tabs[0].url;
+                    url = url.split('/');
+                    var owner = url[3].toString();
+                    var repo = url[4].toString();
+                    var owner_repo = owner + "/" + repo;
+                    var license = null;
+                    var license_dict = null;
+
+
+                    chrome.storage.sync.get("license_dict", function (items) {
+                        if(items.license_dict)
+                        {
+                            license_dict = items.license_dict;
+                            if(license_dict[owner_repo] != undefined)
+                            {
+                                license = license_dict[owner_repo];
+                                chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                            }else{
+                                var apiUrl=  "https://api.github.com/repos/" + owner_repo + "/license";
+                                var json = null;
+                                var xhr = new XMLHttpRequest();
+
+                                xhr.onload = function() {
+                                    if (this.status === 404) {
+                                        // license not found
+                                        license = "notFound";
+                                        chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                                        license_dict[owner_repo] = license;
+                                        chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+
+                                    }}
+                                xhr.open("GET", apiUrl, true);
+                                xhr.onreadystatechange = function() {
+                                    if (xhr.readyState == 4) {
+                                        json = JSON.parse(xhr.response);
+                                        license = json.license;
+                                        chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                                        license_dict[owner_repo] = license;
+                                        chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+                                    }
+                                }
+                                xhr.send();
+                            }
+                        }else{
+                            license_dict = {};
+                            var apiUrl=  "https://api.github.com/repos/" + owner_repo + "/license";
+                            var json = null;
+                            var xhr = new XMLHttpRequest();
+
+                            xhr.onload = function() {
+                                if (this.status === 404) {
+                                    // license not found
+                                    license = "notFound";
+                                    chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                                    license_dict[owner_repo] = license;
+                                    chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+
+                                }}
+                            xhr.open("GET", apiUrl, true);
+                            xhr.onreadystatechange = function() {
+                                if (xhr.readyState == 4) {
+                                    json = JSON.parse(xhr.response);
+                                    license = json.license;
+                                    chrome.tabs.sendMessage(tabs[0].id, {license: license});
+                                    license_dict[owner_repo] = license;
+                                    chrome.storage.sync.set({"license_dict": license_dict}, function () {});
+                                }
+                            }
+                            xhr.send();
+
+                        }
+                    });
+                });
+            }
+        });
+    }
+});
+
---
+++ src/license_alert_contentscript.js
@@ -0,0 +1,42 @@
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+    if(request.license == "limitExceed")
+    {
+
+        if(document.getElementById("license_warning_ext") == null){
+            var htmlWarning = "<div class='flash-messages'>" +
+                "<div id='license_warning_ext'class='flash' style ='background-color: rgba(220,218,61,0.77);color:#fafbfc;'><strong>" +
+                "Github Licenses API limit exceed!</strong></div>";
+            $(".repository-content").prepend(htmlWarning);
+        }
+
+    }
+    if(request.license == "notFound")
+    {
+
+        if(document.getElementById("license_warning_ext") == null){
+            var htmlWarning = "<div class='flash-messages'>" +
+                "<div id='license_warning_ext'class='flash' style ='background-color: rgba(203, 36, 49, 0.77);color:#fafbfc;'><strong>" +
+                "No License found!</strong></div>";
+            $(".repository-content").prepend(htmlWarning);
+        }
+
+    }
+    else if(request.license.featured == false)
+    {
+
+        if(document.getElementById("license_warning_ext") == null){
+            var htmlWarning = "<div class='flash-messages'>" +
+                            "<div id='license_warning_ext'class='flash' style ='background-color: rgba(203, 36, 49, 0.77);color:#fafbfc;'><strong>" +
+                             "License: " + request.license.spdx_id.toString() + "</strong></div></div>";
+            $(".repository-content").prepend(htmlWarning);
+        }
+
+    }
+
+
+});
+
+chrome.runtime.sendMessage({action:"show"});
+//alert("activated!");
