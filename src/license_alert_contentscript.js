1bddc018348eb971c6593e55cf773c5592b485fc
Tugrul Can <tugrulcansollu@gmail.com>
2017-04-15 02:27:02 +0300

    Add limit exceed box

--- src/license_alert_contentscript.js
+++ src/license_alert_contentscript.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (document.getElementById("license_alert") == null) {
         if (request.license === "limitExceed") {
             var htmlWarning = "<div class='flash-messages'>" +
-                "<div id='license_alert'class='flash license_alert limit_exceed'>" +
+                "<div id='license_limit_exceed'class='flash license_alert limit_exceed'>" +
                 "<span class='flash license_alert closebtn'>&times;</span>" +
                 "<strong>Github Licenses API limit exceed!</strong></div>";
             $(".repository-content").prepend(htmlWarning);
@@ -10,7 +10,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
         if (request.license === "notFound") {
             var htmlWarning = "<div class='flash-messages'>" +
-                "<div id='license_alert'class='flash license_alert warning'>" +
+                "<div id='license_not_found'class='flash license_alert warning'>" +
                 "<span class='flash license_alert closebtn'>&times;</span>" +
                 "<strong>No License found!</strong></div>";
             $(".repository-content").prepend(htmlWarning);
