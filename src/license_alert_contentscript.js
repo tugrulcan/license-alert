21a4e15474e2171fb65237d506f5797406c01a01
Tugrul Can <tugrulcansollu@gmail.com>
2017-04-17 22:27:11 +0300

    Background highligthing added

--- src/license_alert_contentscript.js
+++ src/license_alert_contentscript.js
@@ -16,7 +16,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 console.log("inside request.repoInfo.license === 'notFound'");
 
                 var htmlWarning = "<span itemscope='' itemtype='http://schema.org/ListItem' itemprop='itemListElement' id='la-license'>" +
-                    "<a class='js-selected-navigation-item reponav-item' >" +
+                    "<a class='js-selected-navigation-item reponav-item' style='background-color: rgba(255, 0, 0, 0.2);' >" +
                     "<svg aria-hidden='true' class='octicon octicon-law' height='16' version='1.1' viewBox='0 0 14 16' width='14'>" + "" +
                     "<path fill-rule='evenodd' d='M7 4c-.83 0-1.5-.67-1.5-1.5S6.17 1 7 1s1.5.67 1.5 1.5S7.83 4 7 4zm7 6c0 1.11-.89 2-2 2h-1c-1.11 0-2-.89-2-2l2-4h-1c-.55 0-1-.45-1-1H8v8c.42 0 1 .45 1 1h1c.42 0 1 .45 1 1H3c0-.55.58-1 1-1h1c0-.55.58-1 1-1h.03L6 5H5c0 .55-.45 1-1 1H3l2 4c0 1.11-.89 2-2 2H2c-1.11 0-2-.89-2-2l2-4H1V5h3c0-.55.45-1 1-1h4c.55 0 1 .45 1 1h3v1h-1l2 4zM2.5 7L1 10h3L2.5 7zM13 10l-1.5-3-1.5 3h3z'></path></svg>" +
                     "\n<span style='color:#CB2431;'>No License!</span>\n" +
@@ -29,7 +29,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 console.log("inside request.repoInfo.license.featured === false");
 
                 var htmlWarning = "<span itemscope='' itemtype='http://schema.org/ListItem' itemprop='itemListElement' id='la-license'>" +
-                    "<a class='js-selected-navigation-item reponav-item' >" +
+                    "<a class='js-selected-navigation-item reponav-item' style='background-color: rgba(255, 0, 0, 0.2);' >" +
                     "<svg aria-hidden='true' class='octicon octicon-law' height='16' version='1.1' viewBox='0 0 14 16' width='14'>" + "" +
                     "<path fill-rule='evenodd' d='M7 4c-.83 0-1.5-.67-1.5-1.5S6.17 1 7 1s1.5.67 1.5 1.5S7.83 4 7 4zm7 6c0 1.11-.89 2-2 2h-1c-1.11 0-2-.89-2-2l2-4h-1c-.55 0-1-.45-1-1H8v8c.42 0 1 .45 1 1h1c.42 0 1 .45 1 1H3c0-.55.58-1 1-1h1c0-.55.58-1 1-1h.03L6 5H5c0 .55-.45 1-1 1H3l2 4c0 1.11-.89 2-2 2H2c-1.11 0-2-.89-2-2l2-4H1V5h3c0-.55.45-1 1-1h4c.55 0 1 .45 1 1h3v1h-1l2 4zM2.5 7L1 10h3L2.5 7zM13 10l-1.5-3-1.5 3h3z'></path></svg>" +
                     "\n<span style='color:#CB2431;'>" + request.repoInfo.license.spdx_id.toString() + "</span>\n" +
